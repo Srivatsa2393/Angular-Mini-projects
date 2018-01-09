1723d580998270b1ab324dd1153659ac67f39a92
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-10 00:16:52 +0530

    Setting up the routes and mongoose database connection

--- meanauthapp/app.js
+++ meanauthapp/app.js
@@ -4,15 +4,45 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect to database
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+config.database);
+});
+
+//on error
+mongoose.connection.on('error', () => {
+    console.log('Database error: '+err);
+});
 
 const app = express();
 
+const users = require('./routes/users');
+
+//port number
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Invalid endpoints');
-})
+//CORS middlware
+app.use(cors());
 
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//index route
+app.get('/', (req, res) => {
+    res.send('Invalid endpoints by sri');
+});
+
+//start server
 app.listen(port, () => {
     console.log('Server started on port '+port);
-})
+});
