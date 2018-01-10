c41e61f29f3ba7806d16aff05c0ea93b097748e8
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-11 00:19:54 +0530

    Added routing and setup our navbar component

--- meanauthapp/angular-src/src/app/app.module.ts
+++ meanauthapp/angular-src/src/app/app.module.ts
@@ -12,6 +12,14 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +33,8 @@ import { ProfileComponent } from './components/profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
