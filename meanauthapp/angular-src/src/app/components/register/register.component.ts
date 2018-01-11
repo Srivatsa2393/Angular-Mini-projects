b13382dc1750b9038d95b4fd8f01c84437f0adf8
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-11 23:17:52 +0530

    Added the auth service and register User

--- meanauthapp/angular-src/src/app/components/register/register.component.ts
+++ meanauthapp/angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from "../../services/validate.service";
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +16,11 @@ export class RegisterComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor(private validateService: ValidateService, private flashMessagesService: FlashMessagesService) { }
+  constructor(private validateService: ValidateService, 
+              private flashMessagesService: FlashMessagesService,
+              private authService: AuthService,
+              private router: Router
+            ) { }
 
   ngOnInit() {
   }
@@ -41,6 +47,18 @@ export class RegisterComponent implements OnInit {
       this.flashMessagesService.show('Please use a valid email', { cssClass: 'alert-danger', timeout: 3000 });
       return false;
     }
+
+    //register user
+    this.authService.registerUser(user)
+      .subscribe(data => {
+        if(data.success) {
+          this.flashMessagesService.show('You are now registered please login', { cssClass: 'alert-success', timeout: 3000 });
+          this.router.navigate(['/login']);
+        } else {
+          this.flashMessagesService.show('Please register', { cssClass: 'alert-danger', timeout: 3000 });
+          this.router.navigate(['/register']);
+        }
+      });
   }
 
 }
