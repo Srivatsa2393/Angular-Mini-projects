b37f2bf7ca56bd2c0859e8fb26f2e2d23722162f
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-13 20:31:56 +0530

    Setting up the model and connecting to the server

--- contact-list/app.js
+++ contact-list/app.js
@@ -9,6 +9,20 @@ var app = express();
 
 const route = require('./routes/route');
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/contactlist');
+
+//on connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database mongodb at 27017');
+});
+
+mongoose.connection.on('error', (err) => {
+    if(err) {
+        console.log('Error in database connection: '+err);
+    }
+});
+
 //define port no
 const port = 3000;
 
