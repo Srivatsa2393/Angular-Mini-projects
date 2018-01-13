42f36f1196b22966408c35eed1ae1716f823d9e5
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-13 22:43:33 +0530

    Added the code for client side along with the contact service which holds the business logic

---
+++ contact-list/client/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactService } from './contact.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ContactService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
