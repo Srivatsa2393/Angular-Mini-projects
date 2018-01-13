052b0c5f25d683cc28abc19b20de82d966d65754
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-14 00:59:18 +0530

    ading http module in aoo.module

--- contact-list/client/src/app/app.module.ts
+++ contact-list/client/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { ContactService } from './contact.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [ContactService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- contact-list/client/src/app/contacts/contacts.component.ts
+++ contact-list/client/src/app/contacts/contacts.component.ts
@@ -24,7 +24,6 @@ export class ContactsComponent implements OnInit {
         (contacts: any) => {
           this.contacts = contacts;
         }
-      )
+      );
+        }
   }
-
-}
