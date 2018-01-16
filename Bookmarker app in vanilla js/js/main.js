6441f3dbd2d75f9fe24790e0e9fd88a8ab60a2a0
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-17 03:14:41 +0530

    Storing the bookmarks data in localstorage

--- Bookmarker app in vanilla js/js/main.js
+++ Bookmarker app in vanilla js/js/main.js
@@ -1,4 +1,34 @@
 //alert(1);
 
 //listen for form submit
-document.getElementById('#myForm').addEventListener('submit', func)
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+//save bookmark
+function saveBookmark() {
+    //console.log('Bookmark saved');
+    event.preventDefault();
+
+    var siteName = document.getElementById('siteName').value;
+    //console.log(siteName.value);
+
+    var siteUrl = document.getElementById('siteUrl').value;
+    //console.log(siteUrl.value);
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    };
+
+    //console.log(bookmark);
+    //local storage test
+    /* localStorage.setItem('test', 'Hello Srivatsa');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+ */
+
+    if(localStorage.getItem('bookmarks') === null) {
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+}
