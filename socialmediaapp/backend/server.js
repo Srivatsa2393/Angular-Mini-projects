cf165f1587732f8988a0636b1a4be390df685887
Srivatsa S <srivatsashankaran@gmail.com>
2018-01-21 13:57:35 +0530

    Registering a new user with node and mongo

--- socialmediaapp/backend/server.js
+++ socialmediaapp/backend/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var User = require('./models/User');
 
 var app = express();
 
@@ -9,9 +12,35 @@ var posts = [
 ];
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/posts', (req, res) => {
     res.send(posts);
+})
+
+
+app.post('/register', (req, res) => {
+    //console.log(req.body);
+    var userData = req.body;
+    /* console.log(userData.email);
+    res.sendStatus(200); */
+
+
+    var user = new User(userData)
+
+    user.save((err, result) => {
+        if(err){
+            console.log('saving user error');
+        }else{
+            res.sendStatus(200);
+        }
+    })
+})
+
+mongoose.connect('mongodb://test:test@ds111138.mlab.com:11138/pssocial', (err) => {
+    if(!err) {
+        console.log('Connected to mongo');
+    }
 })
 
 app.listen(3000);
